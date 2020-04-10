d39fa2a7410771882f3df0ce7f9f0682c4ef44b7
caetano-felipe <caetano.fbc@gmail.com>
2020-04-09 22:25:30 -0300

    "Corrigindo falha de integração com api-pacientes"

--- assets/js/buscar-pacientes.js
+++ assets/js/buscar-pacientes.js
@@ -5,7 +5,7 @@ botaoAdcionar.addEventListener("click", function(){
 
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientes");
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load", function(){
         var erroAjax = document.querySelector("#erro-ajax");
